1a833f89a5a82f94876225fc0b5c6982d96e3cb5
arif626 <75139432+arif626@users.noreply.github.com>
2024-09-14 15:14:03 +0600

    Delete js directory
    Signed-off-by: arif626 <75139432+arif626@users.noreply.github.com>

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$(".playignitor, .thumbnail").click(function() {
-		const player = videojs('player');
-    	player.src({
-      		src: $(this).attr('data-source'),
-      		type: 'application/x-mpegURL'
-    	});
-	});
-});
